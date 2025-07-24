c92fd0602d114afb36d00bae1c277832a8dff4ed
AlexRiosXI <alexriosxi@gmail.com>
2025-07-23 20:20:14 -0600

    add cors

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from dotenv import load_dotenv
 #from flask_migrate import Migrate
 from utils.db import db
-#from flask_cors import CORS
+from flask_cors import CORS
 
 import os
 import sys
@@ -12,7 +12,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-#CORS(app)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_CONNECTION_STRING")
 
 
